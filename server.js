6b1bca81780e6681149aaa805457273d70d217b5
erikmjohnson <erikarisma98@gmail.com>
2018-12-08 13:41:36 -0800

    setup2

--- server.js
+++ server.js
@@ -1,74 +1,78 @@
 'use strict';
 
-//Application Dependencies
+// Application Dependencies
 const express = require('express');
 const cors = require('cors');
+const request = require('superagent');
 
-//Load env vars;
+// Load env vars;
 require('dotenv').config();
-const PORT = process.env.PORT || 3000; //takes from a .env file and then the terminal env
 
-//app
+const PORT = process.env.PORT || 3000;
+
+// App
 const app = express();
+
 app.use(cors());
 
-// Constructor Functions
-function Location(location){
-  this.formatted_query = location.formatted_address;
-  this.latitude = location.geometry.location.lat;
-  this.longitude = location.geometry.location.lng;  
+// Error handling
+function handleError(res) {
+  res.status(500).send('Sorry something went wrong!');
 }
 
-function Weather(weather) {
-  this.forcast = weather.summary;
-  this.time = weather.time;
-}
-
-// Router
-app.get('/location', getLocation)
-
-app.get('/weather', getWeather)
-
-//Handlers
-function getWeather (request, response) {
-  const weatherData = searchForWeather(request.query)
-  response.send(weatherData);
-}
-
-function getLocation (req, res) {
-  const locationData = searchToLatLong(req.query.data); // 'Lynnwood, WA'
-  res.send(locationData);
-}
-
-function searchForWeather (query) {
-  let weatherData = require('./data/darksky.json');
-  let dailyArray = [];
-  weatherData.daily.data.forEach(forecast => dailyArray.push(new Weather(forecast)));
-  // let convertTime = time.1000
-  for(let i in dailyArray){
-    return {
-      forcast: dailyArray[i].forcast,
-      // time: (dailyArray[i].time*1000.toDateString()
-    };
+// Get location data
+app.get('/location', (req, res) => {
+  const locationData = searchToLatLong(req.query.data || 'lynwood');
+  if (!locationData) {
+    handleError(res);
   }
-}
+  res.send(locationData);
+});
 
-function searchToLatLong(query){
+function searchToLatLong(query) {
   const geoData = require('./data/geo.json');
-  const location = new Location(geoData.results[0]);
+  const location = new Location(query, geoData.results[0]);
   return location;
 }
 
-// Give error messages if incorrect
+function Location(query, location) {
+  this.search_query = query;
+  this.formatted_query = location.formatted_address;
+  this.latitude = location.geometry.location.lat;
+  this.longitude = location.geometry.location.lng;
+}
 
+// Get weather data
+app.get('/weather', (req, res) => {
+  const weatherData = getWeather(req.query.data);
+  if (!weatherData) {
+    handleError(res);
+  }
+  res.send(weatherData);
+});
+
+function getWeather(query) {
+  const weatherJson = require('./data/darksky.json');
+  const weather = new Weather(weatherJson);
+  return weather;
+}
+
+function Weather(weatherJson) {
+  return weatherJson.daily.data.map(day => {
+    return {
+      forecast: day.summary,
+      time: new Date(day.time * 1000).toDateString()
+    }
+  });
+}
+
+// Bad path 
 app.get('/*', function(req, res) {
-  res.status(404).send('Success!');
-})
+  res.status(404).send('You are in the wrong place');
+});
 
-// app.get('/*', function (req, res) {
-//   res.status(500).send('Sorry! Something went horribly wrong!');
-// })
 
+// Listen
 app.listen(PORT, () => {
-  console.log(`app is up on port : ${PORT}`)
-})
+  console.log(`Listening on port: ${PORT}`)}
+);
